cf7023ccc22dc567b30f241dfa9676f4011192af
Oussama Khamlichi <oussama391990@gmail.com>
2019-06-17 02:45:24 +0100

    Update 4chan.sh

--- 4chan.sh
+++ 4chan.sh
@@ -1,25 +1,23 @@
-echo 'red boards only'          
+echo 'Link?'
 
 read l;
-
+ 
 b=$(echo $l | cut  -d/ -f4);
 t=$(echo $l | cut  -d/ -f6);
 
+if [ ! -d "/4chan/$b-$t" ]; then
 
-if [ ! -d "/4chan/$t-$b" ]; then
-
-mkdir -p ./4chan/$t-$b;
-cd ./4chan/$t-$b;
+mkdir -p ./4chan/$b-$t;
+cd ./4chan/$b-$t;
 
 else
 
-cd ./4chan/$t-$b;
+cd ./4chan/$b-$t;
 
 fi
 
-for n in $(curl -sb -H  http://a.4cdn.org/${b}/thread/${t}.json | sed 's/filename/\n/g' | sed 's/time/\n/g' | grep -o ext.* | sed 's/\"\,.*tim\"//;s/ext\"\:\"//g;s/\,\"//g' | sed -E 's/(.*):(.*)/\2\1/'| sed "s|^|https://is2.4chan.org/${b}/|g" | grep '.*jpg\|.*png\|.*gif\|.*webm') ; 
+for n in $(curl -sb -H  http://a.4cdn.org/${b}/thread/${t}.json | sed 's/filename/\n/g' | grep -o '.jpg.*md5\|.png.*md5\|.gif.*md5\|.webm.*md5' | sed 's/\".*\"tim\"//g;s/\,\".*//g' | sed -E 's/(.*):(.*)/\2\1/' | sed "s|^|https://is2.4chan.org/${b}/|g") ;
 
 do wget -nc  $n ; 
 
 done
-
